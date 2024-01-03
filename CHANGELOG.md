92ab9500364ce8d9fd39df12d0f98077d253ba17
Gyorgy Orosz <gyorgy@orosz.link>
2024-01-03 14:04:55 +0100

    fix: handling edge case for previous month computation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
-## [0.3.0] - 2023-12-04
+## [0.3.1] - 2024-01-03
+
+### Fixed
+- Fixed date handling bug in the `export` / `report` command
+
+## [0.3.0] - 2024-01-03
 
 ### Fixed
 - Fixed date handling bug in the `export` / `report` command
--- hours/date_utils.py
+++ hours/date_utils.py
@@ -9,7 +9,10 @@ def first_day_of_prev_month() -> date:
     prev_month = datetime.now().month.numerator - 1
     if prev_month < 1:
         prev_month = 12
-    return datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0, month=prev_month).date()
+    first_day: date = datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0, month=prev_month).date()
+    if prev_month == 12:
+        first_day = first_day.replace(year=first_day.year.numerator - 1)
+    return first_day
 
 
 def tomorrow() -> date:
