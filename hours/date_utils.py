84936f988d0920c6c60cb95be7c98962ef703294
Gyorgy Orosz <gyorgy@orosz.link>
2024-01-03 13:54:37 +0100

    fix: handling edge case for previous month computation

--- hours/date_utils.py
+++ hours/date_utils.py
@@ -6,11 +6,10 @@ def first_day_of_month() -> date:
 
 
 def first_day_of_prev_month() -> date:
-    return (
-        datetime.now()
-        .replace(day=1, hour=0, minute=0, second=0, microsecond=0, month=datetime.now().month.numerator - 1)
-        .date()
-    )
+    prev_month = datetime.now().month.numerator - 1
+    if prev_month < 1:
+        prev_month = 12
+    return datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0, month=prev_month).date()
 
 
 def tomorrow() -> date:
